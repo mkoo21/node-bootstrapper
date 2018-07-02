cc9ab5d3c0064d21c0894902b3407f5df54b570b
Martin <koo.martin@gmail.com>
2018-07-02 14:17:35 -0400

    send DOMstring from server

--- routes.js
+++ routes.js
@@ -15,6 +15,18 @@ const router = (app) => {
     app.get('/echo-:text', (req, res) => {
         res.send(req.params.text);
     });
+
+    app.get('/video', (req, res) => {
+        const html = '<video loop autoplay controls class="background-vid">'
+            + '<source src="static/Build_Our_Machine.mp4" type="video/mp4"></video>';
+        res.send(html);
+    });  
+    app.get('/quote', (req, res) => {
+        const html = '<blockquote class="blockquote text-right">'
+            + '<p class="mb-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. '
+            + 'Integer posuere erat a ante.</p><footer class="blockquote-footer">Martin</footer>';
+        res.send(html);
+    });
 };
 
 module.exports = router;
